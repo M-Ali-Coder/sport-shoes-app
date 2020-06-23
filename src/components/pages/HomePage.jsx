e6690cadc29474d1783aa6a662b383aff2208ce8
M-Ali-Coder <m.ali.coder.93@gmail.com>
2020-06-23 03:22:56 +0200

    Adding HomePageComponent and style it

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import Slider from "../Slider";
+// import Slider from "../Slider";
+import HomePageBanner from "../HomePageBanner";
 
 const HomePage = () => {
   return (
     <div className="home-page">
-      <Slider />
+      {/* <Slider /> */}
+      <HomePageBanner />
     </div>
   );
 };
